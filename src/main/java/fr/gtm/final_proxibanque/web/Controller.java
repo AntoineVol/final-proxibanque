4e8912dfcb497b6b64ceb7f485ba4efffc667081
Steven Roman <veneran_steven@hotmail.com>
2018-07-26 14:05:59 +0200

    Controller

--- src/main/java/fr/gtm/final_proxibanque/web/Controller.java
+++ src/main/java/fr/gtm/final_proxibanque/web/Controller.java
@@ -1,5 +1,16 @@
 package fr.gtm.final_proxibanque.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import fr.gtm.final_proxibanque.business.ClientService;
+
+@org.springframework.stereotype.Controller
+
 public class Controller {
+	
+	@Autowired
+	private ClientService clientService;
+	private ResponseService responseService;
+	
 
 }
