dbda98145b277e41a9623d12f4753d10b42fd569
kamiroudj email <kamirelsisi2017@gmail.com>
2018-07-26 12:59:31 +0200

    ajout des relations

--- src/main/java/fr/gtm/final_proxibanque/domain/Response.java
+++ src/main/java/fr/gtm/final_proxibanque/domain/Response.java
@@ -4,6 +4,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Response {
@@ -16,9 +18,14 @@ public class Response {
 	private boolean positiveResponse;
 	private boolean newClient;
 	
-	
+	@ManyToOne
+	@JoinColumn(name="id_client", referencedColumnName="id")
 	private Client client;
 	
+
+	
+	@ManyToOne
+	@JoinColumn(name="id_survey")
 	private Survey survey;
 	
 	
