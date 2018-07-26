cd0da0faf59e198df28499be64ea1aca0f7bf43d
Steven Roman <veneran_steven@hotmail.com>
2018-07-26 14:36:03 +0200

    update

--- src/main/java/fr/gtm/final_proxibanque/dao/ResponseRepository.java
+++ src/main/java/fr/gtm/final_proxibanque/dao/ResponseRepository.java
@@ -3,7 +3,8 @@ package fr.gtm.final_proxibanque.dao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.mysql.fabric.Response;
+import fr.gtm.final_proxibanque.domain.Response;
+
 
 @Repository
 public interface ResponseRepository extends JpaRepository<Response, Integer>{
