3b4b984da5f76fe74e60991bdf2bfaf79f15e7cc
Steven Roman <veneran_steven@hotmail.com>
2018-07-26 12:07:26 +0200

    update Survey

--- src/main/java/fr/gtm/final_proxibanque/domain/Survey.java
+++ src/main/java/fr/gtm/final_proxibanque/domain/Survey.java
@@ -3,16 +3,24 @@ package fr.gtm.final_proxibanque.domain;
 import java.io.Serializable;
 import java.time.LocalDate;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 /**
  * 
  * @author Kamir, Antoine, Steven
  *
  */
+@Entity
 public class Survey implements Serializable {
 	
 	// Attributes of the Survey class 
 
 	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private LocalDate startDate;
 	private LocalDate endDate;
