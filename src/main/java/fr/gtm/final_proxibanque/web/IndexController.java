9bb5a190fbc52783956178623a5756b3e0228c20
Steven Roman <veneran_steven@hotmail.com>
2018-07-26 14:17:23 +0200

    ajout annotation @Service dans le SurveyService et le ResponseService

--- src/main/java/fr/gtm/final_proxibanque/web/IndexController.java
+++ src/main/java/fr/gtm/final_proxibanque/web/IndexController.java
@@ -1,15 +1,19 @@
 package fr.gtm.final_proxibanque.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
 import fr.gtm.final_proxibanque.business.ClientService;
+import fr.gtm.final_proxibanque.business.ResponseService;
 
-@org.springframework.stereotype.Controller
 
-public class Controller {
+@Controller
+public class IndexController {
 	
 	@Autowired
 	private ClientService clientService;
+	
+	@Autowired
 	private ResponseService responseService;
 	
 
