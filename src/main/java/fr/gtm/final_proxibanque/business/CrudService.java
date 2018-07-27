1abc304a8247b1eb13b2f44c1279fc0d23e0d350
Steven Roman <veneran_steven@hotmail.com>
2018-07-27 15:30:14 +0200

    commentaires

--- src/main/java/fr/gtm/final_proxibanque/business/CrudService.java
+++ src/main/java/fr/gtm/final_proxibanque/business/CrudService.java
@@ -9,23 +9,48 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import fr.gtm.final_proxibanque.domain.MauvaiseDateException;
 
+/**
+ * Le CrudService est un service qu'on met en place pour factoriser le crud pour toutes les entités
+ *
+ * @author Kamir Elsisi, Steven Roman, Antoine Volatron
+ *
+ */
 public abstract class CrudService<ENTITY> {
 
 	@Autowired
 	protected JpaRepository<ENTITY, Integer> repo;
 
+	/**
+	 * La methode create permet créer en base l'entité
+	 *
+	 * @return ça retourne l'entité créée
+	 */
 	public ENTITY create(final ENTITY entity) throws MauvaiseDateException {
 		return this.repo.save(entity);
 	}
 
+	/**
+	 * La methode delete permet supprimer en base l'entité
+	 *
+	 */
 	public void delete(final Integer id) {
 		this.repo.deleteById(id);
 	}
 
+	/**
+	 * La methode getAll permet de récupérer la liste des entités en base
+	 *
+	 *@return ça retourne la liste des entités
+	 */
 	public List<ENTITY> getAll() {
 		return this.repo.findAll();
 	}
 
+	/**
+	 * La methode read permet de récupérer un entité en la récupérant grâce à son identifiant
+	 *
+	 *@return ça retourne l'entité
+	 */
 	public ENTITY read(final Integer id) {
 		ENTITY entity = null;
 		final Optional<ENTITY> opt = this.repo.findById(id);
@@ -35,6 +60,11 @@ public abstract class CrudService<ENTITY> {
 		return entity;
 	}
 
+	/**
+	 * La methode update permet mettre à jour l'entité
+	 *
+	 *@return ça retourne l'entité
+	 */
 	public ENTITY update(final ENTITY entity) {
 		return this.repo.save(entity);
 	}
--- src/main/java/fr/gtm/final_proxibanque/domain/Response.java
+++ src/main/java/fr/gtm/final_proxibanque/domain/Response.java
@@ -1,5 +1,7 @@
 package fr.gtm.final_proxibanque.domain;
 
+import java.io.Serializable;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -15,7 +17,12 @@ import javax.persistence.ManyToOne;
  *
  */
 @Entity
-public class Response {
+public class Response implements Serializable {
+
+	/**
+	 * Clé de serialisation
+	 */
+	private static final long serialVersionUID = 1L;
 
 	/**
 	 * Le client qui a répondu au sondage positivement
@@ -48,62 +55,104 @@ public class Response {
 	private boolean positiveResponse;
 
 	/**
-	 * Le sondage en question-
+	 * Le sondage en question
 	 */
 	@ManyToOne
 	@JoinColumn(name = "id_survey")
 	private Survey survey;
+	
+	
 
-	public Response() {
-
-	}
-
+	/**Getter du Client
+	 * @return the client, retourne le client
+	 */
 	public Client getClient() {
-		return this.client;
+		return client;
 	}
 
-	public String getComment() {
-		return this.comment;
-	}
-
-	public Integer getId() {
-		return this.id;
-	}
-
-	public Survey getSurvey() {
-		return this.survey;
-	}
-
-	public boolean isNewClient() {
-		return this.newClient;
-	}
-
-	public boolean isPositiveResponse() {
-		return this.positiveResponse;
-	}
-
-	public void setClient(final Client client) {
+	/**
+	 * @param client the client to set
+	 */
+	public void setClient(Client client) {
 		this.client = client;
 	}
 
-	public void setComment(final String comment) {
+	/**
+	 * Getter du comment
+	 * @return the comment, le commentaire
+	 */
+	public String getComment() {
+		return comment;
+	}
+
+	/**
+	 * @param comment the comment to set
+	 */
+	public void setComment(String comment) {
 		this.comment = comment;
 	}
 
-	public void setId(final Integer id) {
+	/**
+	 * Getter de l'id
+	 * @return the id, identifiant unique de la réponse
+	 */
+	public Integer getId() {
+		return id;
+	}
+
+	/**
+	 * 
+	 * @param id the id to set
+	 */
+	public void setId(Integer id) {
 		this.id = id;
 	}
 
-	public void setNewClient(final boolean newClient) {
+	/**
+	 * Getter du newClient
+	 * @return the newClient, qui informe si le client est nouveau ou pas
+	 */
+	public boolean isNewClient() {
+		return newClient;
+	}
+
+	/**
+	 * @param newClient the newClient to set
+	 */
+	public void setNewClient(boolean newClient) {
 		this.newClient = newClient;
 	}
 
-	public void setPositiveResponse(final boolean positiveResponse) {
+	/**
+	 * Getter du positiveResponse
+	 * @return the positiveResponse, qui informe si la réponse est positive
+	 */
+	public boolean isPositiveResponse() {
+		return positiveResponse;
+	}
+
+	/**
+	 * @param positiveResponse the positiveResponse to set
+	 */
+	public void setPositiveResponse(boolean positiveResponse) {
 		this.positiveResponse = positiveResponse;
 	}
 
-	public void setSurvey(final Survey survey) {
+	/**
+	 * Getter du survey
+	 * @return the survey, qui est le sondage
+	 */
+	public Survey getSurvey() {
+		return survey;
+	}
+
+	/**
+	 * @param survey the survey to set
+	 */
+	public void setSurvey(Survey survey) {
 		this.survey = survey;
 	}
+
+	
 
 }
